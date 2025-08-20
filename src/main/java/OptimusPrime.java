db90689115266a018ccab72faba3b68ef45c16ef
nihaalmanaf <nihaalmanaf@gmail.com>
2025-08-20 15:18:53 +0800

    Added functionality where optimus prime echos user

--- src/main/java/OptimusPrime.java
+++ src/main/java/OptimusPrime.java
@@ -1,16 +1,35 @@
+import java.util.Scanner;
+
 public class OptimusPrime {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         String line = "-----------------------------------------------";
-        String greetText = "Hello! I'm Optimus Prime\nWhat can I do for you?";
-        String byeText = "Bye. Hope to see you again soon!";
+        String greetText = "Hello! I'm Optimus Prime, Leader of the Autobots\nLWhat can I do for you?";
+        String byeText = "Autobots, Roll Out!";
 
         System.out.println(line);
         System.out.println(greetText);
         System.out.println(line);
-        System.out.println(byeText);
-        System.out.println(line);
+
+
+        while(true){
+            System.out.print("User: ");
+            String input = scanner.nextLine();
+            System.out.println(line);
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(byeText);
+                System.out.println(line);
+                break;
+            } else {
+                System.out.println("Optimus Prime: " +  input);
+                System.out.println(line);
+            }
+        }
+
+
 
     }
 }
