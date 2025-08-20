b8e3f1b42b0131f10413526363227c9be882a974
nihaalmanaf <nihaalmanaf@gmail.com>
2025-08-20 15:03:56 +0800

    Changed Name to Optrimus Prime and implemented greet and exit

---
+++ src/main/java/OptimusPrime.java
@@ -0,0 +1,16 @@
+public class OptimusPrime {
+
+    public static void main(String[] args) {
+
+        String line = "-----------------------------------------------";
+        String greetText = "Hello! I'm Optimus Prime\nWhat can I do for you?";
+        String byeText = "Bye. Hope to see you again soon!";
+
+        System.out.println(line);
+        System.out.println(greetText);
+        System.out.println(line);
+        System.out.println(byeText);
+        System.out.println(line);
+
+    }
+}
