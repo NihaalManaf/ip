948564085827eb0719e5f88a2e76ecf65f96a4fb
nihaalmanaf <nihaalmanaf@gmail.com>
2025-08-21 16:03:10 +0800

    added CreateTask method which dynamically creates task based on command

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class TaskList {
 
     Task[] list = new Task[100];
@@ -9,6 +11,36 @@ public class TaskList {
     public void addToList(Task task){
         list[pointer] = task;
         pointer ++;
+    }
+
+    public String createTask(String taskName, String metadata){
+
+        Task task;
+
+        if (Objects.equals(taskName, "todo")) {
+            task = new Todos(taskName);
+        } else if(Objects.equals(taskName, "deadline")) {
+            String subString = "/by";
+            String finalDate = metadata.substring(metadata.indexOf(subString));
+            task = new Deadlines(taskName, finalDate);
+        } else if(Objects.equals(taskName, "event")) {
+            String firstSubString = "/from";
+            String secondSubString = "/to";
+            String startDate = metadata.substring(
+                    metadata.indexOf(firstSubString),
+                    metadata.indexOf(secondSubString)
+            );
+            String endDate = metadata.substring(metadata.indexOf(secondSubString));
+            task = new Events(taskName, startDate, endDate);
+        } else {
+            return "Error in reading task!";
+        }
+        list[pointer] = task;
+        pointer ++;
+
+        return "Got it. I've added this task:\n"
+                + task.toString() + "\n"
+                + "Now you have " + pointer +" tasks in the list.";
     }
 
     public Task markComplete(int i){
