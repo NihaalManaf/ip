b1bafff605cadeb57acd443291cf51fdbed448ec
nihaalmanaf <nihaalmanaf@gmail.com>
2025-09-02 11:26:10 +0800

    Implement testing for the TaskList.java class and test createTask method and deleteTask methods

--- src/test/java/optimusprime/parser/ParserTest.java
+++ src/test/java/optimusprime/parser/ParserTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
